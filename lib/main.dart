324aedc427628361bd07d3d1ac49418303d68de2
Adam Hammer <adamhammer@gmail.com>
2020-01-19 11:52:46 -0800

    reduced the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clock_helper/customizer.dart';
-import 'clocks/space/space_clock.dart';
-import 'clocks/ticker/ticker_clock.dart';
+import 'clocks/clock_scaffolding.dart';
 
 void main() {
-  //We have no inputs, so Fuschia should be fine all around
+  /// Fuschia should run fine all around here
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeRight,
-    DeviceOrientation.landscapeLeft,
-  ]);
   runApp(const MainWidget());
 }
 
@@ -29,35 +22,4 @@ class MainWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ClockCustomizer(
       (model) => Builder(builder: (context) => ClockScaffolding(model: model)));
-}
-
-///
-/// The scaffolding of this clock
-///
-/// It's a Stack
-/// Layer 1: The space scene
-/// Layer 2: The DateTimeAndWeatherTicker the draws in the top left
-///
-class ClockScaffolding extends StatelessWidget {
-  /// Construct a clock scaffolding given a model
-  const ClockScaffolding({@required this.model, Key key}) : super(key: key);
-
-  /// The ClockModel, needed by things to make decisions about what to draw
-  final ClockModel model;
-
-
-  @override
-  Widget build(BuildContext context) => Stack(
-        children: <Widget>[
-          //The space clock
-          SpaceClockScene(model),
-
-          Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: DateTimeAndWeatherTicker(clockModel: model),
-              )),
-        ],
-      );
 }
